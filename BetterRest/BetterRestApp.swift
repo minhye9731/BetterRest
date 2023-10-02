7c436f82e3bf1ff2f40fbcf6bec277296292e59f
minhye9731 <jenney62639731@gmail.com>
2023-10-02 22:46:50 +0900

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by 강민혜 on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
